04cb8bec518b7c0f39b53820805f36f5c0b94a15
Incle <Mr.incle@gmail.com>
2013-06-01 17:20:22 +0400

    Create Homework 2_Queue.cpp

---
+++ Homework 2_Queue.cpp
@@ -0,0 +1,89 @@
+/** 
+  @file	Queue.cpp
+  @brief	Example of queue.
+*/
+#include <iostream>
+#include <ctime>
+using namespace std;
+/** 
+	@struct	queue
+	@brief	Structure for queue
+  @param value	Value of element
+	@param *next	Link of next element
+*/
+struct queue
+{
+	int value;
+	queue *next;
+} *first, *last;								/// first and last elements
+/** 
+	@fn		void add(int value)
+  @brief	Add element to queue
+	@param value	Value of the element
+	@return			
+*/
+void add (int value)
+{
+	queue *ex = new queue;
+	ex->value = value;
+	ex->next = NULL;
+	if(first)
+	{
+		last->next = ex;
+		last = ex;
+	}
+	else
+	{
+		first = ex;
+		last = first;
+	}
+	cout<<"\n Add "<<value;
+}
+/**
+	@fn		void print()
+  @brief	Print queue in order
+	@return	
+*/
+void print()
+{
+  queue *ex;
+	ex = first;
+	cout<<"\nQueue\n";
+	while (ex)
+	{
+		cout<<" "<<ex->value;
+		ex = ex->next;
+	}
+}
+/**
+	@fn		void deleteElements()
+  @brief	Delete elements of queue
+	@return	
+*/
+void deleteElements()
+{   
+	queue *ex;
+	while(first)
+	{
+		ex = first->next;
+		delete first;
+		first = ex;
+	}
+	cout<<"\nQueue was deleted\n";
+}
+void main()
+{
+	int size, ex;
+	srand(time(0));
+	cout<<"Input number of elements ";
+	cin>>size;
+	cout<<"\nCreat";
+	while (--size)
+	{
+		ex = rand()%10000;
+		add(ex);
+	}
+	print();							    /// print
+	deleteElements();					/// delete
+	getchar();
+}
